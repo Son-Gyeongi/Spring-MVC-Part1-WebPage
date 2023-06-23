a41b215ff90a3dc7cb3fd7bbba058a8f85ec4d0d
Son-Gyeongi <thsruddl77@gmail.com>
2023-06-24 00:09:27 +0900

    extra. 상품 등록 폼 - 타임리프로 렌더링 - 컨트롤러(BasicItemController) addForm(상품 등록 폼), save(상품 등록 저장)메서드 - 뷰 템플릿(addForm.html)

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -47,6 +48,23 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item"; // 뷰로 간다.
+    }
+
+    /**
+     * 상품 등록 폼
+     * addForm.html의 form을 열 때는 GET 실제 저장할 때는 POST
+     * 같은 url이지만 http method로 기능을 구분해준다.
+     */
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm"; // 뷰로 간다.
+    }
+    /**
+     * 상품 등록 저장
+     */
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
 
     /**
